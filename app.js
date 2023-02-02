da6b292551250d062c6e457b39e0d3303247189f
Yun Jang <jang.yunj7@gmail.com>
2023-02-02 15:33:30 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("hello world");
+
+let firstName;
+let lastName;
+var age = 18;
+
+firstName = "yun";
+lastName = "jang";
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Invisible Man";
+let author = "Ralph Ellison";
+let pageCount = 480;
+let hasRead = true;
